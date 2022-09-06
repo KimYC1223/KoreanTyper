b41eef9acaa9564ad897113a991841560a103661
KimYC1223 <kau_esc@naver.com>
2022-09-06 17:47:13 +0900

    update auto typer demo scene

---
+++ Assets/KoreanTyper/DemoScene/KoreanTyperDemo_Auto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KoreanTyper;
+using UnityEngine.UI;
+using KoreanTyper;
+
+public class KoreanTyperDemo_Auto : MonoBehaviour {
+    public Text[] TestTexts;
+
+    private void Start() {
+        StartCoroutine(TypingText());
+    }
+
+    public IEnumerator TypingText() {
+        while (true) {
+            string[] strings = new string[3]{ "20XX년 X월 X일 X요일 오후 HH시 MM분",
+                                              "유니티 한글 타이퍼 오토 타이핑 데모 씬",
+                                              "이 데모는 자동으로 작성되고 있습니다." };
+            foreach (Text t in TestTexts)
+                t.text = "";
+
+            for (int t = 0; t < TestTexts.Length && t < strings.Length; t++) {
+                int strTypingLength = strings[t].GetTypingLength();
+
+                for (int i = 0; i <= strTypingLength; i++) {
+                    TestTexts[t].text = strings[t].Typing(i);
+                    yield return new WaitForSeconds(0.03f);
+                }
+                yield return new WaitForSeconds(1f);
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
+
---
+++ Assets/KoreanTyper/DemoScene/KoreanTyperDemo_Simple.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KoreanTyper;
+using UnityEngine.UI;
+
+public class KoreanTyperDemo_Simple : MonoBehaviour {
+    public Text TestText;
+    public Slider TestSldier;
+
+    private void Start() {
+        OnTestSliderChange();
+    }
+
+    public void OnTestSliderChange() {
+        TestText.text = "2022년 어느 한 여름날.\n난 그 날을 잊을 수 없다.".Typing(TestSldier.value);
+    }
+}
